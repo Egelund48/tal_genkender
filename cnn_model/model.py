13935b16a7dff81699b8712f5a517e4586db7d77
Egelund48 <79721682+Egelund48@users.noreply.github.com>
2022-12-16 10:21:21 +0100

    Add files via upload

--- cnn_model/model.py
+++ cnn_model/model.py
@@ -1,1 +1,54 @@
 
+import numpy as np
+import cv2
+import keras
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPool2D
+from keras import optimizers
+from keras.datasets import mnist
+from keras.utils import to_categorical
+
+#load data 
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
+_,X_train_th = cv2.threshold(X_train,127,255,cv2.THRESH_BINARY)
+_,X_test_th = cv2.threshold(X_test,127,255,cv2.THRESH_BINARY)
+
+X_train = X_train_th.reshape(-1,28,28,1)
+X_test = X_test_th.reshape(-1,28,28,1)
+
+y_train = to_categorical(y_train, num_classes = 10)
+y_test = to_categorical(y_test, num_classes = 10)
+
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+
+#model 
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1)))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
+
+model.summary()
+
+history = model.fit(X_train, y_train,epochs=5, shuffle=True,
+                    batch_size = 200,validation_data= (X_test, y_test))
+
+
+model.save('model_data.h5')
